fa956a2562c31e59d4fdb94acdde1b9992b4aa85
miniChorri <christofercosta_8@hotmail.com>
2025-10-04 10:25:26 -0500

    feat: added initial configurations for primevue theme

--- src/main.js
+++ src/main.js
@@ -1,5 +1,37 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Aura from '@primeuix/themes/aura';
+import PrimeVue from 'primevue/config';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import {
+    Toolbar,
+    Button
+} from "primevue";
+import {definePreset} from "@primeuix/themes";
 
-createApp(App).mount('#app')
+const MyPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{sky.50}',
+            100: '{sky.100}',
+            200: '{sky.200}',
+            300: '{sky.300}',
+            400: '{sky.400}',
+            500: '{sky.500}',
+            600: '{sky.600}',
+            700: '{sky.700}',
+            800: '{sky.800}',
+            900: '{indigo.900}',
+            950: '{indigo.950}'
+        }
+    }
+});
+createApp(App)
+    .use(PrimeVue, {theme:{preset:MyPreset}}
+    )
+    .component('pv-toolbar', Toolbar)
+    .component('pv-button',Button)
+
+    .mount('#app')
