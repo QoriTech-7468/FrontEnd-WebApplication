c67efed4a23b1aa621eb02b7299b03cd2387e008
Yaku Guzman <yakumgc12@gmail.com>
2025-10-08 15:56:26 -0500

    feat(route-planning-execution): integrate i18n support and add Toast and Confirmation services

--- src/main.js
+++ src/main.js
@@ -3,11 +3,12 @@ import './style.css'
 import App from './App.vue'
 import Aura from '@primeuix/themes/aura';
 import PrimeVue from 'primevue/config';
+import i18n from './i18n.js';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
 import {
     Toolbar,
-    Button, Toast, ConfirmDialog
+    Button, Toast, ConfirmDialog, ConfirmationService, ToastService
 } from "primevue";
 import {definePreset} from "@primeuix/themes";
 import router from "./router.js";
@@ -33,10 +34,17 @@ createApp(App)
     .use(PrimeVue, {theme:{preset:MyPreset}}
     )
     .use(router)
+    .use(i18n)
     .component('pv-toolbar', Toolbar)
     .component('pv-button',Button)
 
 
     .component('pv-confirm-dialog', ConfirmDialog)
     .component('pv-toast', Toast)
+    .use(ToastService)
+    .use(ConfirmationService)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-button', Button)
+    .component('pv-toast', Toast)
+    .component('pv-confirm-dialog', ConfirmDialog)
     .mount('#app')
