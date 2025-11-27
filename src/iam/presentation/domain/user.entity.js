0bc64004b4c92cfa1560924f7e34419dbd4450a9
Jesús Castillo Vidal <jcdev2004@gmail.com>
2025-11-27 18:00:23 -0500

    fix(IAM): added sign-in-command, sign-up-command and user.entity

---
+++ src/iam/presentation/domain/user.entity.js
@@ -0,0 +1,8 @@
+export class User {
+    constructor({ id, name, surname, organizationId }) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.organizationId = organizationId;
+    }
+}
